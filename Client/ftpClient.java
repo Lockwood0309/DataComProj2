b4a954756dea05327ebfc7034f950bb28dc9e227
Lockwood <lockwood@Lockwoods-MacBook-Pro.local>
2021-11-11 17:52:25 -0500

    Forgot to save before commit

--- Client/ftpClient.java
+++ Client/ftpClient.java
@@ -2,6 +2,12 @@ package Client;
 
 // Handles keyword search with centralized server
 
+// Handles:
+// Connection with centralized Server
+// keyword search with centralized server
+// Get Command with another ftpClient
+
 public class ftpClient {
     
+    // Establish connection with 
 }
