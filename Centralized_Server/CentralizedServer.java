9c6f0c8d7f5ec5952aba0b955507a6827d3b32c8
OzymandiasB <bominrahmani@gmail.com>
2021-11-22 15:18:39 -0500

    finished

--- Centralized_Server/CentralizedServer.java
+++ Centralized_Server/CentralizedServer.java
@@ -1,9 +1,17 @@
 package Centralized_Server;
 
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 import java.util.StringTokenizer;
+import java.util.concurrent.locks.Lock;
+import java.util.*;
+import java.util.concurrent.locks.*;
 
 import javax.management.loading.PrivateClassLoader;
+import javax.sound.sampled.Port;
 
 import java.io.*;
 
@@ -12,63 +20,285 @@ import java.io.*;
 // Maintain a table for user's files & descriptors (I think we should use dictionaries for this => Dictionary of Type (ID,List))
 
 // Commands
-// Connect: Establish connection w/ host. Once init connection has been made, tell host ready to recieve filenames & descriptors 
+// Connect: Establish connection w/ host. Once init connection has been made, tell host ready to recieve filenames & descriptors
 // KeywordSearch: Search the file table for the keyword. Return the user, hostname, & connection speed to host (Returns the all locations of the file/descriptor)
 
-public class CentralizedServer extends Thread {
+public class CentralizedServer{
+    private Dictionary<Integer,ArrayList<String>> creds = new Hashtable<>();
+    private Dictionary<Integer,ArrayList<String>> file_info = new Hashtable<>();
 
-    private Socket connSocket;
+    private Lock creds_lock = new ReentrantLock();
+    private Lock file_info_lock = new ReentrantLock();
 
-    private DataOutputStream outToClient;
-    private BufferedReader inFromHost;
-
-    private String username;
-    private String hostname;
-    private String connSpeed;
-
-    public CentralizedServer(Socket connSocket){
-        this.connSocket = connSocket;
-    }
-
-    // Sets up initial connection with a host & obtains the host's credentials
-    private void initConnection(){
-        try{
-            outToClient = new DataOutputStream(connSocket.getOutputStream());
-            inFromHost = new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
-
-            StringTokenizer tokens = new StringTokenizer(inFromHost.readLine());
-            username = tokens.nextToken();
-            hostname = tokens.nextToken();
-            connSpeed = tokens.nextToken();
-        }catch(Exception e){
-            System.out.println("Error Occured while Establishing a new connection");
-        }
-
-    }
-
-    // Establish Connection with host, let host known connection has been made, retireve filenames and descriptor,
-    // wait for commands (keyword, close Connection) from host
-    public void run(){
-        initConnection();
-    }
+    int client_count = 0;
 
     // Wait for new connection. Create new thread to handle connection once established
-    private static void newConnection(ServerSocket serverSocket){
-            CentralizedServer server = new CentralizedServer(serverSocket.accept());
-            Thread clientHandler = new Thread(server);
-            clientHandler.start();
+    private void newConnection(Integer ID, ServerSocket serverSocket) throws Exception{
+        host_handler new_host_handler = new host_handler(ID,serverSocket.accept());
+        client_count += 1;
+        System.out.println("connected");
+        Thread hostHandler = new Thread(new_host_handler);
+        hostHandler.start();
+    }
+
+    // Main Centralized server loop. Creates a new connection by calling newConnection and then increments the ID counter after a connection has been made
+    public void start_server(){
+        try{
+            Integer ID = 0;
+            ServerSocket serverSocket = new ServerSocket(1200);
+
+            while(true){
+                newConnection(ID,serverSocket);
+                ID += 1;
+            }
+
+        }catch(Exception e){
+            System.err.println("Could not listen on port: 1200. Error: " + e.getMessage());
+            System.exit(-1);
+        }
     }
 
     // Start Centralized Server
     public static void main(String[] args){
-        try{
-            ServerSocket serverSocket = new ServerSocket(1200);
-            
-            while(true){
-                newConnection(serverSocket);
+        CentralizedServer main_server = new CentralizedServer();
+        main_server.start_server();
+    }
+
+    // Inner Thread class to handle diff host connections while still being able to access global shared vars in the outer class
+    public class host_handler extends Thread{
+        int port;
+        Integer my_ID; // The hosts id associate w/ the connection
+
+        Socket connSocket;
+        DataOutputStream outToHost;
+        DataInputStream inFromHost;
+
+
+        host_handler(Integer ID, Socket connSocket){
+            try{
+                this.my_ID = ID;
+                this.connSocket = connSocket;
+                outToHost = new DataOutputStream(connSocket.getOutputStream());
+                inFromHost = new DataInputStream(connSocket.getInputStream());
+            }catch(Exception e){
+                System.err.println("Could not connect wih " + ID.toString() + ": " + connSocket.getInetAddress().toString() + ". Error: " + e.getMessage());
+
             }
-        }catch(IOException e){
-            System.err.println("Could not listen on port: 1200.");
-            System.exit(-1);
         }
+
+        // Establish Connection with host, let host known connection has been made, retireve filenames and descriptor,
+        // wait for commands (keyword, close Connection) from host
+        public void run(){
+            try{
+
+                set_host_creds(get_host_creds());
+                set_host_file_info(get_host_file_info());
+                host_sends_command();
+            }catch(Exception e){
+                System.err.println("Could not get the host credentials. Error3: " + e.getMessage());
+                // Error has occured and the thread will now end with the host. The output statment has been handled by the function that threw it.
+            }
+        }
+
+        // Gets the host creds and returns them as an ArrayList
+        private ArrayList<String> get_host_creds() throws Exception{
+            System.out.println("host_creds");
+            ArrayList<String> host_cred = new ArrayList<String>(){{
+                try{
+                    outToHost.writeUTF(String.valueOf(client_count));
+
+                    String line = " ";
+                    System.out.println("Line: " + line);
+                    StringTokenizer tokens = new StringTokenizer(line = inFromHost.readUTF());
+                    System.out.println("Line: " + line);
+                    add(tokens.nextToken()); // username
+                    add(tokens.nextToken()); // hostname
+                    add(tokens.nextToken()); // connection speed
+                    add(tokens.nextToken()); // port
+                    System.out.println("Line: " + line);
+                }catch(Exception e){
+                    System.err.println("Could not get the host credentials. Error2: " + e.getMessage());
+                    throw new Exception();
+                }
+            };};
+
+
+            System.out.println(host_cred.get(0));
+
+            System.out.println(host_cred.get(1));
+
+            System.out.println(host_cred.get(2));
+
+            return host_cred;
+        }
+
+        // Accesses gloabl outer: lock cred var to insure no data collusion
+        private void set_host_creds(ArrayList<String> cred){
+            creds_lock.lock();
+            creds.put(my_ID, cred);
+            creds_lock.unlock();
+        }
+
+        // Gets the host file info and returns them as an ArrayList
+        private ArrayList<String> get_host_file_info() throws Exception{
+            ArrayList<String> host_file_info = new ArrayList<String>(){{
+                try {
+                    String data;
+                    while(!(data = inFromHost.readUTF()).equals("eof") ){
+                        add(data);
+                    }
+                } catch (Exception e) {
+                    System.err.println("Could not get the hosts file info. Error: " + e.getMessage());
+                    throw new Exception();
+                }
+            };};
+
+            for(int i=0; i<host_file_info.size(); i++){
+                System.out.println(host_file_info.get(i));
+            }
+
+            return host_file_info;
+        }
+
+        private void set_host_file_info(ArrayList<String> filenames) throws Exception{
+            file_info_lock.lock();
+            file_info.put(my_ID, filenames);
+            file_info_lock.unlock();
+        }
+
+        private void host_sends_command() throws Exception{
+            try{
+                while(true){
+                    String line;
+                    if(!(line = inFromHost.readUTF()).equals("eof")){
+                        String cmd;
+                        StringTokenizer tokens = new StringTokenizer(line);
+                        cmd = tokens.nextToken();
+                        //System.out.println("cmd: "+cmd);
+                        if(cmd.equals("close")){
+                            close_connection();
+                            break;
+                        }else if(cmd.equals("keyword")){                                          // cmd must equal keyword search (Check for input error on Client end)
+                            port = Integer.parseInt(tokens.nextToken());
+                            keyword_search(tokens.nextToken());
+                        }else{
+                            System.out.println("wtf");
+                        }
+                    }
+                }
+            }catch(Exception e){
+                System.err.println("Could resolve host command. Error: " + e.getMessage());
+                throw new Exception(e.getMessage());
+            }
+        }
+
+        // Searches the outer classes file_info dictionary for any file_info that matches keyword
+        private void keyword_search(String keyword) throws Exception{
+            //print_tables();
+            try {
+                Socket dataSocket = new Socket(connSocket.getInetAddress(), port);
+                DataOutputStream outData = new DataOutputStream(dataSocket.getOutputStream());
+
+                // Search For files in dictionary => send them back to the host
+
+                ArrayList<Integer> file_keys = new ArrayList<Integer>(Collections.list(file_info.keys()));
+
+                for (Integer info : file_keys) {
+                    for(String in : file_info.get(info)){
+                        if(in.contains(keyword)){
+                            // return to client hostname port filename connSpeed
+
+                            String hostname = creds.get(info).get(1); // creds.get(i).get(1) => hostname
+                            String port = creds.get(info).get(3);
+                            String filename = in; // file_info.get(i).get(0) => filename
+                            String connSpeed = creds.get(info).get(2); // creds.get(i).get(2) => connSpeed
+
+                            System.out.println(hostname + " " + port + " " + filename + " " + connSpeed);
+
+                            outData.writeUTF(hostname + " " + port + " " + filename + " " + connSpeed);
+                        }
+                    }
+                }
+                // for(int i = 0; i < file_info.size(); i++){
+                //     for(String info : file_info.get(i)){
+                // if(info.contains(keyword)){
+                //     // return to client hostname port filename connSpeed
+
+                //     String hostname = creds.get(i).get(1); // creds.get(i).get(1) => hostname
+                //     String port = creds.get(i).get(3);
+                //     String filename = info; // file_info.get(i).get(0) => filename
+                //     String connSpeed = creds.get(i).get(2); // creds.get(i).get(2) => connSpeed
+
+                //     System.out.println(hostname + " " + port + " " + filename + " " + connSpeed);
+
+                //     outData.writeUTF(hostname + " " + port + " " + filename + " " + connSpeed);
+                // }
+                //     }
+                // }
+                outData.writeUTF("eof");;
+            } catch (Exception e) {
+                // Do nothing
+                System.out.println("Error " + e.getMessage());
+            }
+        }
+
+        // Closes all connections with the host assciated with my_ID
+        private void close_connection(){
+            try{
+                System.out.println("before");
+                creds_lock.lock();
+                creds.remove(my_ID);
+                creds_lock.unlock();
+
+                file_info_lock.lock();
+                file_info.remove(my_ID);
+                file_info_lock.unlock();
+
+                System.out.println("after");
+
+                print_tables();
+
+                outToHost.close();;
+                inFromHost.close();
+
+                connSocket.close();
+            }catch(Exception e){
+                System.out.println("Error when closing connection: " + e.getMessage());
+            }
+        }
+
+        private void print_tables(){
+
+            ArrayList<Integer> creds_keys = new ArrayList<Integer>(Collections.list(creds.keys()));
+
+            for (Integer cred : creds_keys) {
+                for(String info : creds.get(cred)){
+                    System.out.println("creds["+cred+"]: " + info);
+                }
+            }
+
+            ArrayList<Integer> file_keys = new ArrayList<Integer>(Collections.list(file_info.keys()));
+
+            for (Integer info : file_keys) {
+                for(String in : file_info.get(info)){
+                    System.out.println("creds["+info+"]: " + in);
+                }
+            }
+            // for(Integer cred_key : creds.keys()){
+
+            // }
+            // for(int i =0; i<creds.size(); i++){
+            //     for(String cred : creds.get(i)){
+            //         System.out.println("creds["+i+"]: " + cred);
+            //     }
+            // }
+
+            // for(int i =0; i<file_info.size(); i++){
+            //     for(String info : file_info.get(i)){
+            //         System.out.println("fileinfo["+i+"]: " + info);
+            //     }
+            // }
+        }
+
+    }
 }
--- Client/ftpClient.java
+++ Client/ftpClient.java
@@ -1,13 +1,251 @@
 package Client;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import javax.sound.midi.SysexMessage;
+
 // Handles keyword search with centralized server
 
 // Handles:
 // Connection with centralized Server
 // keyword search with centralized server
-// Get Command with another ftpClient
+// Get Command with another ftpServer
+
 
 public class ftpClient {
-    
-    // Establish connection with 
+
+    Socket ControlSocket;
+    Socket PeerControlSocket;
+    int port = 1200;
+
+    int port1 = 1221;
+
+    int connPort;
+    String ip;
+
+    DataOutputStream outToServer;
+    DataInputStream inFromServer;
+
+    DataOutputStream P2PoutToServer;
+    DataInputStream P2PinFromServer;
+
+    Dictionary<Integer,ArrayList<Object>> keyword_search_table;
+
+    Host my_host;
+    Host_GUI my_GUI;
+
+    public ftpClient(Host host){
+        my_host = host;
+    }
+
+    public void set_GUI(Host_GUI GUI){
+        my_GUI = GUI;
+    }
+
+    public Dictionary<Integer,ArrayList<Object>> get_file_table(){
+        return keyword_search_table;
+    }
+
+    public void connect_to_server(String ip, int port, String username, String hostname, String connSpeed){
+        try{
+            my_GUI.output_text.append(">> Connect to Centralized Server"+"\n");
+            this.ip = ip;
+            ControlSocket = new Socket(ip, port);
+            // Display connection to output window
+
+            outToServer = new DataOutputStream(ControlSocket.getOutputStream());
+            inFromServer = new DataInputStream(ControlSocket.getInputStream());
+            //BufferedReader inFromClient = new BufferedReader(new InputStreamReader(ControlSocket.getInputStream()));
+
+            int client_count = Integer.parseInt(inFromServer.readUTF());
+
+            connPort = port1 + (2*client_count);
+
+            // Set host creds
+            System.out.println(username+ " " + hostname + " " + connSpeed);
+            outToServer.writeUTF(username+ " " + hostname + " " + connSpeed + " " + connPort);
+
+
+            File dir = new File(System.getProperty("user.dir"));
+            File[] files = dir.listFiles();
+
+            if(files == null){
+                outToServer.writeUTF("eof");
+            }else{
+                for(File file : files){
+                    outToServer.writeUTF(file.getName());
+                }
+                outToServer.writeUTF("eof");
+            }
+            my_GUI.output_text.append("\t Connected to Centralized Server"+"\n");
+            my_host.start_ftpserver();
+        }catch(Exception e){
+            my_GUI.output_text.append("\t Error: " + e.getMessage());
+        }
+    }
+
+    public void keyword_search(String keyword){
+        try{
+            //System.out.println("key " + keyword);
+
+            port += 2;
+            ServerSocket welcomeData = new ServerSocket(port);
+            outToServer.writeUTF("keyword " + port + " " + keyword);
+
+            my_GUI.output_text.append(">> Keyword " + keyword+"\n");
+            my_GUI.output_text.append("\t keyword " + port + " " + keyword+"\n");
+            //System.out.println("key: " + keyword);
+
+            Socket dataSocket = welcomeData.accept();
+
+            DataInputStream inData = new DataInputStream(new BufferedInputStream(dataSocket.getInputStream()));
+
+            keyword_search_table =  new Hashtable<Integer,ArrayList<Object>>();
+
+            int count = 0;
+            while(true){
+                String sentence = inData.readUTF();
+                if(sentence.equals("eof")){
+                    break;
+                }
+                StringTokenizer tokens = new StringTokenizer(sentence);
+
+                String hostname = tokens.nextToken();
+                int port = Integer.parseInt(tokens.nextToken());
+                String filename = tokens.nextToken();
+                String connSpeed = tokens.nextToken();
+
+                keyword_search_table.put(count,new ArrayList<>(){{
+                    add(hostname);
+                    add(port);
+                    add(filename);
+                    add(connSpeed);
+                }});
+                count++;
+            }
+
+            System.out.println(keyword_search_table.size());
+            my_GUI.display_file_table(keyword_search_table);
+
+            welcomeData.close();
+        }catch(Exception e){
+            // Display error to output window
+            my_GUI.output_text.append("\t Error: " + e.getMessage()+"\n");
+        }
+    }
+
+    public void connect_to_peer(String cmd){
+        try{
+            StringTokenizer tokens = new StringTokenizer(cmd);
+            String my_cmd = tokens.nextToken();
+            String connIP = tokens.nextToken();
+            String connP = tokens.nextToken();
+
+            //System.out.println("\n" + cmd);
+
+            System.out.println(my_cmd);
+            System.out.println(connIP);
+            System.out.println(connP);
+
+            PeerControlSocket = new Socket(connIP, Integer.parseInt(connP));
+
+            P2PoutToServer = new DataOutputStream(PeerControlSocket.getOutputStream());
+            P2PinFromServer = new DataInputStream(PeerControlSocket.getInputStream());
+            my_GUI.output_text.append("Connected with peer"+"\n");
+        }catch(Exception e){
+            my_GUI.output_text.append("\t Error: " + e.getMessage()+"\n");
+        }
+    }
+
+    public void retr_from_peer(String cmd){
+        try{
+            int tempConnPort = connPort - 1;
+            //System.out.println("before");
+            ServerSocket welcomeData = new ServerSocket(tempConnPort);
+            //System.out.println("after");
+
+            P2PoutToServer.writeUTF(String.valueOf(tempConnPort) + " " + cmd);
+
+            StringTokenizer tokens = new StringTokenizer(cmd);
+            String my_cmd = tokens.nextToken();
+            String my_filename = tokens.nextToken();
+
+            Socket dataSocket = welcomeData.accept();
+            //System.out.println("after2");
+            File file = new File(my_filename);
+
+            if(file.createNewFile()){
+                FileWriter fileW = new FileWriter(file);
+                DataInputStream inData = new DataInputStream(dataSocket.getInputStream());
+                String line;
+                if((line = inData.readUTF()).equals("200")){
+                    while(true){
+                        line = inData.readUTF();
+                        if(line.equals("eof")){
+                            fileW.close();
+                            break;
+                        }else{
+                            fileW.write(line,0,line.length());
+                        }
+                    }
+                    my_GUI.output_text.append("\t File has been retrieved"+"\n");
+                }else{
+                    my_GUI.output_text.append("Server Counldn't get " + my_filename + " " + line+"\n");
+                    file.delete();
+                }
+            }
+            dataSocket.close();
+            welcomeData.close();
+            // retr file
+        }catch(Exception e){
+            my_GUI.output_text.append("\t Error: " + e.getMessage()+"\n");
+        }
+    }
+
+    public void close_peer_connection(){
+        try{
+            P2PoutToServer.writeUTF("close");
+            // P2PinFromServer.close();
+            // P2PoutToServer.close();
+            my_GUI.output_text.append("\t Connection has been closed with peer"+"\n");
+        }catch(Exception e){
+            my_GUI.output_text.append("\t Error: " + e.getMessage()+"\n");
+        }
+    }
+
+    // Socket ControlSocket;
+    // Socket PeerControlSocket;
+    // int port = 1200;
+
+    // int port1 = 1221;
+
+    // int connPort;
+    // String ip;
+
+    // DataOutputStream outToServer;
+    // DataInputStream inFromServer;
+    public void close_server_connection(){
+        try{
+            my_GUI.output_text.append(">> close");
+            outToServer.writeUTF("close");
+            my_GUI.output_text.append("\t Closing Connection with Server...");
+            System.out.println("\t Closing Connection with Server...");
+
+            if(PeerControlSocket != null && PeerControlSocket.isConnected()){
+                close_peer_connection();
+                PeerControlSocket.close();
+            }
+            outToServer.close();
+            inFromServer.close();
+            ControlSocket.close();
+        }catch(Exception e){
+            my_GUI.output_text.append("\t Error with disconection: " + e.getMessage()+"\n");
+            System.out.println("\t Error with disconection: " + e.getMessage()+"\n");
+        }
+
+
+    }
+
 }
